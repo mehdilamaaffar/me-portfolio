c83f6dc4b98d76d82cd1fbe6a8eec856e3e82a9b
mehdi lamaaffar <mehdilamaafar@gmail.com>
2018-03-05 23:20:43 +0000

    hot fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if (env('REDIRECT_HTTPS')) {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 
